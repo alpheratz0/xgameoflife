9c4cf3755ebc1c882340a084b578ffc08eebacfc
alpheratz0 <alpheratz99@protonmail.com>
2022-10-16 20:22:44 -0300

    fix openbsd build

--- config.mk
+++ config.mk
@@ -4,9 +4,10 @@
 VERSION   = 0.1.0
 
 CC        = cc
-CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os -DVERSION=\"$(VERSION)\"
+INCS      = -I/usr/X11R6/include
+CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
 LDLIBS    = -lxcb -lxcb-cursor -lxcb-keysyms -lxcb-xkb -lm
-LDFLAGS   = -s
+LDFLAGS   = -L/usr/X11R6/lib -s
 
 PREFIX    = /usr/local
 MANPREFIX = $(PREFIX)/share/man

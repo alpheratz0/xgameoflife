02d2c696b9b60be586ba7dd471fe95ebe803c9ed
alpheratz0 <alpheratz99@protonmail.com>
2024-04-02 09:26:21 -0300

    bump version to 0.2.1

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
-# Copyright (C) 2022-2023 <alpheratz99@protonmail.com>
+# Copyright (C) 2022-2024 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION = 0.2.0
+VERSION = 0.2.1
 
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
